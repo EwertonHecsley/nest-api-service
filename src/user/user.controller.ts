3f6bda910edc5e7ab19767de0323f0749fef12ea
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-01-21 13:34:39 -0300

    aplicando no controller os servicos de hash

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service'
 import { Response } from 'express';
 import { IUser } from './dtos/User.dto';
+import { HashPasswordService } from './services/HashPassword.service';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) { };
+    constructor(
+        private readonly userService: UserService,
+        private readonly userHashPasswordService: HashPasswordService
+    ) { };
 
     @Post()
     async create(@Body() body: IUser, @Res() res: Response) {
@@ -14,7 +18,9 @@ export class UserController {
         const verifyEmail = await this.userService.getUserByEmail(email);
         if (verifyEmail) return res.status(HttpStatus.BAD_REQUEST).json({ mensagem: 'Email já cadastrado no sistema.' });
 
-        const user = await this.userService.create({ name, email, password });
+        const hashPassword = await this.userHashPasswordService.hashPassword(password);
+
+        const user = await this.userService.create({ name, email, password: hashPassword });
         return res.status(HttpStatus.CREATED).json({ mensagem: 'Usuário cadastrado com sucesso.', usuario: user });
     };
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { HashPasswordService } from './services/HashPassword.service';
 
 @Module({
-  providers: [UserService],
+  providers: [UserService, HashPasswordService],
   controllers: [UserController]
 })
-export class UserModule {}
+export class UserModule { }
