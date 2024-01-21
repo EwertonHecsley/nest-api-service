8f7ec894a0e45499f4d260c83597f4231256a323
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-01-21 14:07:17 -0300

    adiconando validcoes ao DTo de usuario

--- src/user/dtos/User.dto.ts
+++ src/user/dtos/User.dto.ts
@@ -1,6 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, isEmail } from "class-validator"
+
 export class IUser {
     id?: number
+
+    @IsString()
+    @IsNotEmpty({ message: 'Campo name não pode ser vazio.' })
     name: string
+
+    @IsString()
+    @IsNotEmpty({ message: 'Campo email não pode ser vazio.' })
+    @IsEmail()
     email: string
+
+    @IsString()
+    @IsNotEmpty({ message: 'Campo password não pode ser vazio.' })
+    @MinLength(4)
     password: string
-}
+};
