a6522f9cfd62666b08eb9b99a7ebd5504325a6cc
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2024-01-21 13:22:00 -0300

    metodos do service e rota create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service'
 import { Response } from 'express';
+import { IUser } from './dtos/User.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) { };
 
-    @Get()
-    getAll(@Res() res: Response) {
-        return res.json(this.userService.getHello())
-    }
+    @Post()
+    async create(@Body() body: IUser, @Res() res: Response) {
+        const { name, email, password } = body;
+
+        const verifyEmail = await this.userService.getUserByEmail(email);
+        if (verifyEmail) return res.status(HttpStatus.BAD_REQUEST).json({ mensagem: 'Email já cadastrado no sistema.' });
+
+        const user = await this.userService.create({ name, email, password });
+        return res.status(HttpStatus.CREATED).json({ mensagem: 'Usuário cadastrado com sucesso.', usuario: user });
+    };
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import knex from '../database/connection';
+import { IUser } from './dtos/User.dto';
 
 @Injectable()
 export class UserService {
-    getHello(): string {
-        return 'Hello World! ';
-    }
-}
+    async create(user: IUser): Promise<IUser[]> {
+        return await knex('user').insert(user).returning('*');
+    };
+
+    async getAll(): Promise<IUser[]> {
+        return await knex('user');
+    };
+
+    async getUserById(id: number): Promise<IUser> {
+        return await knex('user').where({ id }).first();
+    };
+
+    async getUserByEmail(email: string): Promise<IUser> {
+        return await knex('user').where({ email }).first();
+    };
+};
